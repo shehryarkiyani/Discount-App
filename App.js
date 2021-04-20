2f927a289b3a380c63b974816938626c6e893505
shehryar kiyani <shehryarkiani121@gmail.com>
2021-04-20 16:56:03 +0500

    Task 3

--- App.js
+++ App.js
@@ -1,17 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React ,{useState,useEffect}from 'react';
 import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
+  const[t1,setT1]=useState('')
+  const[t2,setT2]=useState('')
+  const[save,setSave]=useState('')
+  const[original,setOriginal]=useState('')
+  const setPrice=(txt)=>{
+setT2(txt)
+
+  }
+  useEffect(() => {
+    
+    calculate()
+  },[t2]);
+  const calculate=()=>{
+    let val1=parseInt(t1)
+    let val2=parseInt(t2)
+    let newprice=(val1*val2)/100
+    let saveprice=val1-newprice
+    if(t2===''){
+setSave('')
+    setOriginal('')
+    }else{
+      setSave(saveprice.toString())
+    setOriginal(newprice.toString())
+      console.log(saveprice)
+    }
+    
+  }
   return (
     <View style={styles.container}>
       <View style={styles.innerContainer}>
-      <TextInput style={styles.TextInputStyle} placeholder="Enter Original Price"/>
-      <TextInput style={styles.TextInputStyle} placeholder="Enter Discount Percentage"/>
-
+        <View>
+          <Text>Original Price</Text>
+          <TextInput onChangeText={(txt)=>setT1(txt)} style={styles.TextInputStyle} placeholder="Enter Original Price"/>
+      
+        </View>
+        <View>
+          <Text>Discount: </Text>
+          <TextInput onChangeText={setPrice} style={styles.TextInputStyle} placeholder="Enter Discount Percentage"/>
+     
+        </View>
+        <View>
+          <Text>You Save</Text>
+          <TextInput style={styles.TextInputStyle} value={save}/>
+        </View>
+        <View>
+          <Text>New Price</Text>
+          <TextInput style={styles.TextInputStyle}  value={original}/>
+        </View>
+     
+      
       </View>
       
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -32,6 +74,7 @@ const styles = StyleSheet.create({
     borderTopWidth:0,
     borderRightWidth:0,
     borderLeftWidth:0,
-    width:'100%'
+    width:'100%',
+    height: 60
   },
 });
